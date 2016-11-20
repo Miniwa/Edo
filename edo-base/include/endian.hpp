62f143de8f045dd08e43882b52047b87fd8f9885
miniwa <blarch3030@gmail.com>
2016-11-20 18:01:30 +0100

    Added endian module to base lib

---
+++ edo-base/include/endian.hpp
@@ -0,0 +1,71 @@
+#ifndef EDO_ENDIAN_HPP
+#define EDO_ENDIAN_HPP
+
+#include <boost/endian/conversion.hpp>
+
+namespace edo
+{
+    enum class endianness
+    {
+        big = boost::endian::order::big,
+        little = boost::endian::order::little,
+        native = boost::endian::order::native,
+    };
+
+    /// Returns the big-endian representation of a given native value
+    template<typename T>
+    T native_to_big(T value)
+    {
+        return boost::endian::native_to_big(value);
+    }
+
+    /// Returns the little-endian representation of a given native value
+    template<typename T>
+    T native_to_little(T value)
+    {
+        return boost::endian::native_to_little(value);
+    }
+
+    /// Returns the given endianness representation of a given native value
+    template<typename T>
+    T native_to_order(T value, endianness order)
+    {
+        if(order == endianness::little)
+        {
+            return native_to_little(value);
+        }
+        else
+        {
+            return native_to_big(value);
+        }
+    }
+
+    /// Returns the native representation of a given big-endian value
+    template<typename T>
+    T big_to_native(T value)
+    {
+        return boost::endian::big_to_native(value);
+    }
+
+    /// Returns the native representation of a given little-endian value
+    template<typename T>
+    T little_to_native(T value)
+    {
+        return boost::endian::little_to_native(value);
+    }
+
+    /// Returns the native representation of a given value in given endianness
+    template<typename T>
+    T order_to_native(T value, endianness order)
+    {
+        if(order == endianness::little)
+        {
+            return little_to_native(value);
+        }
+        else
+        {
+            return big_to_native(value);
+        }
+    }
+}
+#endif
---
+++ tests/edo-base/src/endian_test.cpp
@@ -0,0 +1,79 @@
+#include <boost/test/unit_test.hpp>
+#include "endian.hpp"
+
+struct EndianFixture
+{
+    EndianFixture()
+    {
+        native_i = 1;
+        big_i = boost::endian::native_to_big(native_i);
+        little_i = boost::endian::native_to_little(native_i);
+    }
+
+    uint32_t big_i;
+    uint32_t little_i;
+    uint32_t native_i;
+};
+
+BOOST_FIXTURE_TEST_SUITE(endian_tests, EndianFixture)
+
+BOOST_AUTO_TEST_CASE(test_endianness_enum)
+{
+    bool little = edo::endianness::little == edo::endianness::native;
+    bool big = edo::endianness::big == edo::endianness::native;
+    BOOST_REQUIRE(little != big);
+}
+
+BOOST_AUTO_TEST_CASE(test_native_to_big)
+{
+    BOOST_REQUIRE_EQUAL(edo::native_to_big(native_i), big_i);
+}
+
+BOOST_AUTO_TEST_CASE(test_native_to_little)
+{
+    BOOST_REQUIRE_EQUAL(edo::native_to_little(native_i), little_i);
+}
+
+BOOST_AUTO_TEST_CASE(test_native_to_order_little)
+{
+    BOOST_REQUIRE_EQUAL(
+        edo::native_to_order(native_i, edo::endianness::little),
+        little_i
+    );
+}
+
+BOOST_AUTO_TEST_CASE(test_native_to_order_big)
+{
+    BOOST_REQUIRE_EQUAL(
+        edo::native_to_order(native_i, edo::endianness::big),
+        big_i
+    );
+}
+
+BOOST_AUTO_TEST_CASE(test_big_to_native)
+{
+    BOOST_REQUIRE_EQUAL(edo::big_to_native(big_i), native_i);
+}
+
+BOOST_AUTO_TEST_CASE(test_little_to_native)
+{
+    BOOST_REQUIRE_EQUAL(edo::little_to_native(little_i), native_i);
+}
+
+BOOST_AUTO_TEST_CASE(test_order_to_native_little)
+{
+    BOOST_REQUIRE_EQUAL(
+        edo::order_to_native(little_i, edo::endianness::little),
+        native_i
+    );
+}
+
+BOOST_AUTO_TEST_CASE(test_order_to_native_big)
+{
+    BOOST_REQUIRE_EQUAL(
+        edo::order_to_native(big_i, edo::endianness::big),
+        native_i
+    );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
