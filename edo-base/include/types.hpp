ae949ccdb889ace2103ee7746f854cf34872b311
miniwa <blarch3030@gmail.com>
2016-11-21 23:52:00 +0100

    Added process types to base

--- edo-base/include/types.hpp
+++ edo-base/include/types.hpp
@@ -5,8 +5,19 @@
 
 namespace edo
 {
+    /// An 8-bit byte
     using byte = uint8_t;
+
+    /// A memory address
     using memaddr = uintptr_t;
+
+    /// A memory offset
     using memoffset = intptr_t;
+
+    /// A process handle
+    using hproc = void*;
+
+    /// A process id
+    using pid = uint32_t;
 }
 #endif
