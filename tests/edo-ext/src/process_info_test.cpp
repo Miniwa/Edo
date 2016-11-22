05e341e89dadb80e66adae8e24b25c71db4d3eb0
miniwa <blarch3030@gmail.com>
2016-11-22 23:30:24 +0100

    Created edo-ext tests

---
+++ tests/edo-ext/src/process_info_test.cpp
@@ -0,0 +1,17 @@
+#include <boost/test/unit_test.hpp>
+#include "process_info.hpp"
+
+BOOST_AUTO_TEST_SUITE(process_info_tests)
+
+BOOST_AUTO_TEST_CASE(test_constructor_defaults)
+{
+    edo::ProcessInfo pi = edo::ProcessInfo("test.exe", 10, 1, 5);
+
+    BOOST_REQUIRE_EQUAL(pi.exe_name, "test.exe");
+    BOOST_REQUIRE_EQUAL(pi.process_id, 10);
+    BOOST_REQUIRE_EQUAL(pi.parent_process_id, 1);
+    BOOST_REQUIRE_EQUAL(pi.thread_count, 5);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
